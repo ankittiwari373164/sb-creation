3b4549212cd9ceeb9c5a42006eb1c803b60757a1
ankittiwari373164 <ankittiwari.373164@gmail.com>
2026-04-24 16:58:38 +0530

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -113,7 +113,7 @@ const Footer = () => {
         </div>
 
         {/* 📜 Bottom Bar */}
-        <div className="mt-12 pt-8 border-t border-gray-200 flex flex-col md:flex-row justify-between items-center gap-4">
+        <div className="mt-12 pt-8 border-t border-gray-200 flex flex-col md:flex-row justify-between items-center gap-4 pb-7">
           <p className="text-xs text-gray-500">
             © {new Date().getFullYear()} SB Creation. All rights reserved.
           </p>
